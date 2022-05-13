ca2c977d546953780620c58fe79076c878d983bf
Mario González <mario@mariogl.com>
2022-05-13 09:05:03 +0200

    3

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { program } = require("commander");
 const initializeServer = require("./server");
 
-const port = process.env.SERVER_PORT ?? 4000;
+const port = process.env.SERVER_PORT || process.env.PORT || 4000;
+console.log(port);
 program.option("-n, --name <name>", "Name");
 
 program.parse();
